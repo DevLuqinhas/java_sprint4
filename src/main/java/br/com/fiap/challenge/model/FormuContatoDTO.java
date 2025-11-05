d0168a1cdb5ef690a5622513072a7cbf028fc8ee
LucasVieira <lucasfigueiredovieira1234@gmail.com>
2025-11-05 19:02:23 -0300

    inclusao formuContatoDTO.java

---
+++ src/main/java/br/com/fiap/challenge/model/FormuContatoDTO.java
@@ -0,0 +1,47 @@
+package br.com.fiap.challenge.model;
+
+public class FormuContatoDTO {
+
+    private String nome;
+    private String email;
+    private String mensagem;
+
+    public FormuContatoDTO() {}
+
+    public FormuContatoDTO(String nome, String email, String mensagem) {
+        this.nome = nome;
+        this.email = email;
+        this.mensagem = mensagem;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    // Validação simples
+    public boolean isValid() {
+        return nome != null && !nome.isBlank()
+                && email != null && email.contains("@")
+                && mensagem != null && !mensagem.isBlank();
+    }
+}
